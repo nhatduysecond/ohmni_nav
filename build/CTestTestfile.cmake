09c770a8232a2c73e1fbaea2f87a18d9bca09b39
nhatduyfirst <nhatduyfirst@gmail.com>
2022-06-23 04:58:48 +0700

    nav robot with amcl

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/nhatduyfirst/catkin_ws/src/ohmnibot
+# Build directory: /home/nhatduyfirst/catkin_ws/src/ohmnibot/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/nhatduyfirst/catkin_ws/src/ohmnibot/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/nhatduyfirst/catkin_ws/src/ohmnibot/build/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/nhatduyfirst/catkin_ws/src/ohmnibot/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/nhatduyfirst/catkin_ws/src/ohmnibot/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/nhatduyfirst/catkin_ws/src/ohmnibot:$ROS_PACKAGE_PATH"
